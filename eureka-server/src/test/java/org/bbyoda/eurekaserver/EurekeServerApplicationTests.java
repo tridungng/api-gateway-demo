dfe0646b1c9945e550acd5f81ba8ba18fafbd6e8
bbyoda <h2trungvuong@gmail.com>
2025-05-17 12:19:50 +0200

    feat: create eureka-server module

---
+++ eureka-server/src/test/java/org/bbyoda/eurekaserver/EurekeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bbyoda.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
